078cbb4bda8f7706ac513ccb213b522a62e20db4
Raissa Hohenester <raissa.developer@gmail.com>
2018-08-15 13:12:25 +0200

    Assync actions and more middlewares

--- src/app/actions/userActions.js
+++ src/app/actions/userActions.js
@@ -6,14 +6,22 @@
 } */
 
 export function setName(name){
-    return dispatch => {
+    /* return dispatch => {
         setTimeout(()=> {
         dispatch({
             type: "SET_NAME",
             payload: name
         });
         }, 2000);
-    }
+    } */
+    return {
+        type: "SET_NAME",
+        payload: new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(name);
+            }, 2000);
+        })
+    };
 }
 
 export function setAge(number){
