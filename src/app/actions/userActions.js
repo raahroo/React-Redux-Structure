a23b2757050ab679cff60557529d48b3bca24563
Raissa Hohenester <raissa.developer@gmail.com>
2018-08-15 12:57:41 +0200

    Starting with async Actions and installed the middleware redux-thunk

--- src/app/actions/userActions.js
+++ src/app/actions/userActions.js
@@ -1,7 +1,18 @@
-export function setName(name){
+/* export function setName(name){
     return {
         type: "SET_NAME",
         payload: name
+    }
+} */
+
+export function setName(name){
+    return dispatch => {
+        setTimeout(()=> {
+        dispatch({
+            type: "SET_NAME",
+            payload: name
+        });
+        }, 2000);
     }
 }
 
